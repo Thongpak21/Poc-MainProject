c659bf74843d10a51ed4bf436f079ffb4db95eca
Thongpak Pongsilathong <thongpak.pon@truedigital.com>
2020-07-24 15:03:47 +0700

    initial project

---
+++ MainProject/MainProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  MainProject
+//
+//  Created by Thongpak Pongsilathong on 24/7/2563 BE.
+//  Copyright © 2563 Thongpak Pongsilathong. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
